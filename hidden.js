978895437ad53a3efd730c80de462a10c0c7ccf2
merp101 <34143165+merp101@users.noreply.github.com>
2018-12-11 13:00:56 -0500

    Update hidden.js

--- hidden.js
+++ hidden.js
@@ -1,9 +1,9 @@
 var cheater = false;
 var mult = false;
 body.addEventListener('keypress', function() {
-   var x = event.keyCode;
+   var x = event.charCode;
    console.log(x);
-   if (event.keyCode == 13) { //enter
+   if (x == 13) { //enter
        if (document.getElementById("capitalLocation").value == "Helsinki" || document.getElementById("capitalLocation").value == "helsinki") {alert("you win")}
    } else if (x === 123 || x === 17 || x === 122 || x === 116 || x === 112 || x === 113 || x === 114 || x === 115) { // f1, f2, f3, f4, f5, f11, f12
      if (!cheater) {
