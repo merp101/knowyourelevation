0a56f926970dcf82eec43186246c2524e11ae6ea
merp101 <34143165+merp101@users.noreply.github.com>
2019-01-08 11:22:58 -0500

    Update timer.js

--- timer.js
+++ timer.js
@@ -6,24 +6,31 @@ if (mult == undefined) {
   mult = false;
 }
 
+function incTimer() {
+  ms += 1;
+  if (ms == 250) {
+     if (sec == 1) {
+        sec = 59;
+        min -= 1;
+     } else { 
+        sec -= 1;
+        document.getElementById("TtimerS").innerHTML = sec;
+        document.getElementById("TtimerM").innerHTML = min;
+        ms = 1;
+     }
+	}
+  document.getElementById("Etimer").innerHTML = elevation.toFixed(1) + " feet";
+  elevation -= (13/200) * ((mult) ? 1000 : 1);
+}
+  
+
 function loop() {
   if (elevation <= 0) {
       document.getElementById("body").style.backgroundColor="black";
       document.getElementById("container").display="none";
     } else {
-      ms += 1;
-      if (ms == 250) {
-         if (sec == 1) {
-           sec = 59;
-           min -= 1;
-          } else sec -= 1;
-          document.getElementById("TtimerS").innerHTML = sec;
-          document.getElementById("TtimerM").innerHTML = min;
-          ms = 1;
-       }
-     document.getElementById("Etimer").innerHTML = elevation.toFixed(1) + " feet";
-     elevation -= (13/200) * ((mult) ? 1000 : 1);
-     }
+      incTimer();
+    }
 }
 
 setInterval(
