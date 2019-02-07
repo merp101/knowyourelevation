a4d6a4eeabaab81fcc6971d3ceb296bd70bc2389
merp101 <34143165+merp101@users.noreply.github.com>
2019-02-07 11:45:07 -0500

    Update hidden.js

--- hidden.js
+++ hidden.js
@@ -4,13 +4,17 @@ body.addEventListener('keydown', function() {
    var x = event.keyCode;
    console.log(x);
    if (x == 13) { //enter
-       if (document.getElementById("capitalLocation").value == "Helsinki" || document.getElementById("capitalLocation").value == "helsinki") {alert("you win")}
+       if (document.getElementById("capitalLocation").value == "Helsinki" || document.getElementById("capitalLocation").value == "helsinki" || document.getElementById("capitalLocation").value == "HELSINKI") {showRiddle();}
    } else if (x === 123 || x === 17 || x === 122 || x === 116 || x === 112 || x === 113 || x === 114 || x === 115) { // f1, f2, f3, f4, f5, f11, f12
      if (cheater < 2) {
-       alert("it worked");//document.getElementById("cheaterWarning").display="block"; 
+       alert("No cheating!");
        cheater += 1;
        
      }
    }
    if (cheater == 2) {mult = true}
 });
+function showRiddle() {
+   document.getElementById("letter").display="block";
+   document.getElementById("topLeftStuff").display="none";
+}
