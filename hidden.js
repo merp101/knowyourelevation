9b3529b161ee603f946285e8cf8e44ac6fa77090
merp101 <34143165+merp101@users.noreply.github.com>
2019-02-20 11:41:11 -0500

    Update hidden.js

--- hidden.js
+++ hidden.js
@@ -1,6 +1,5 @@
 body.addEventListener('keydown', function() {
    var x = event.keyCode;
-   console.log(x);
    if (x == 13) { //enter
        if (document.getElementById("capitalLocation").value == "Helsinki" || document.getElementById("capitalLocation").value == "helsinki" || document.getElementById("capitalLocation").value == "HELSINKI") {showThing();}
    }
